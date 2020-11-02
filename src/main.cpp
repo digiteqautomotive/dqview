3d692c80849305109896c6a636ffa1e02e930d8c
Martin Tuma <martin.tuma@digiteqautomotive.com>
2020-11-02 11:40:49 +0100

    Set the VLC plugin path to something usable on Windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
+#include <QtGlobal>
 #include "application.h"
 
 int main(int argc, char *argv[])
 {
+#if defined(Q_OS_WIN32) || defined(Q_OS_CYGWIN)
+	qputenv("VLC_PLUGIN_PATH", "plugins";
+#endif
+
 	Application app(argc, argv);
 	return app.run();
 }
