010ee725d21794004613b1f4f2e5b2e2b86e85e0
Martin Tuma <martin.tuma@digiteqautomotive.com>
2020-11-02 14:11:15 +0100

    Enable console output on Windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <QtGlobal>
+#if defined(Q_OS_WIN32) || defined(Q_OS_CYGWIN)
+#include <Windows.h>
+#endif
 #include "application.h"
 
 int main(int argc, char *argv[])
 {
 #if defined(Q_OS_WIN32) || defined(Q_OS_CYGWIN)
+	if (AttachConsole(ATTACH_PARENT_PROCESS)) {
+		freopen("CONOUT$", "w", stdout);
+		freopen("CONOUT$", "w", stderr);
+	}
+
 	qputenv("VLC_PLUGIN_PATH", "plugins");
 #endif
 
