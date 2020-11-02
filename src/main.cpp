6b7824ad96473d97577b06f064ff9acb11d2c338
Martin Tuma <martin.tuma@digiteqautomotive.com>
2020-11-02 11:45:41 +0100

    Fixed typo

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
 #if defined(Q_OS_WIN32) || defined(Q_OS_CYGWIN)
-	qputenv("VLC_PLUGIN_PATH", "plugins";
+	qputenv("VLC_PLUGIN_PATH", "plugins");
 #endif
 
 	Application app(argc, argv);
