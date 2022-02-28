a0dff3c9be929dc0ef9cca97643bf1ed7ae7ec97
Martin Tuma <martin.tuma@digiteqautomotive.com>
2022-02-28 19:10:10 +0100

    Added HiDPI support

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@ int main(int argc, char *argv[])
 	qputenv("VLC_PLUGIN_PATH", "plugins");
 #endif
 
+	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+	QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+
 	Application app(argc, argv);
 	return app.run();
 }
