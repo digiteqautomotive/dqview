5600c29f5f5382e1dfdcbb202d867b33143f8002
Martin Tůma <martin.tuma@digiteqautomotive.com>
2023-05-12 14:29:16 +0200

    Only create the camera config once, not on everz URL request

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,40 +1,53 @@
 #include <QtGlobal>
 #include "camera.h"
-#if defined(Q_OS_WIN32) || defined(Q_OS_CYGWIN)
-#include "fg4.h"
-#endif
 
 #if defined(Q_OS_WIN32) || defined(Q_OS_CYGWIN)
-QPoint Camera::resolution() const
+QSize Camera::resolution() const
 {
 	long resolution;
 
-	IFG4InputConfig *config = (IFG4InputConfig*)device().config();
-	if (!config)
-		return QPoint();
-	bool err = FAILED(config->GetDetectedResolution(&resolution));
-	config->Release();
-
-	return err ? QPoint() : QPoint(resolution >> 16, resolution & 0xFFFF);
+	if (!_config)
+		return QSize();
+	return (FAILED(_config->GetDetectedResolution(&resolution)))
+	  ? QSize() : QSize(resolution >> 16, resolution & 0xFFFF);
 }
 #endif
 
 QString Camera::url() const
 {
 	QString url;
+
 #if defined(Q_OS_LINUX)
 	url = QString("v4l2://%1").arg(_cameraInfo.device().name());
 #elif defined(Q_OS_WIN32) || defined(Q_OS_CYGWIN)
-	QPoint r(resolution());
-	if (r.isNull())
+	QSize r(resolution());
+
+	if (r.width() > 0 && r.height() > 0)
+		url = QString("dshow:// :dshow-vdev=%1 :dshow-adev=none :dshow-aspect-ratio=%2")
+		  .arg(_cameraInfo.device().name(), QString::number(r.width()) + ":"
+		  + QString::number(r.height()));
+	else
 		url = QString("dshow:// :dshow-vdev=%1 :dshow-adev=none")
 		  .arg(_cameraInfo.device().name());
-	else
-		url = QString("dshow:// :dshow-vdev=%1 :dshow-adev=none :dshow-aspect-ratio=%2")
-		  .arg(_cameraInfo.device().name(), QString::number(r.x()) + ":"
-		  + QString::number(r.y()));
 #else
 #error "unsupported platform"
 #endif
+
 	return url;
+}
+
+Camera::Camera(const DeviceInfo &cameraInfo, QObject *parent)
+  : Video(parent), _cameraInfo(cameraInfo)
+{
+#if defined(Q_OS_WIN32) || defined(Q_OS_CYGWIN)
+	_config = (IFG4InputConfig*)device().config();
+#endif
+}
+
+Camera::~Camera()
+{
+#if defined(Q_OS_WIN32) || defined(Q_OS_CYGWIN)
+	if (_config)
+		_config->Release();
+#endif
 }
--- src/camera.h
+++ src/camera.h
@@ -2,17 +2,20 @@
 #define CAMERA_H
 
 #include <QUrl>
-#include <QPoint>
+#include <QSize>
 #include "deviceinfo.h"
 #include "video.h"
+#if defined(Q_OS_WIN32) || defined(Q_OS_CYGWIN)
+#include "fg4.h"
+#endif
 
 class Camera : public Video
 {
 	Q_OBJECT
 
 public:
-	Camera(const DeviceInfo &cameraInfo, QObject *parent = 0)
-	  : Video(parent), _cameraInfo(cameraInfo) {}
+	Camera(const DeviceInfo &cameraInfo, QObject *parent = 0);
+	~Camera();
 
 	QString url() const;
 	QString name() const {return _cameraInfo.name();}
@@ -20,7 +23,9 @@ public:
 
 private:
 #if defined(Q_OS_WIN32) || defined(Q_OS_CYGWIN)
-	QPoint resolution() const;
+	QSize resolution() const;
+
+	IFG4InputConfig *_config;
 #endif
 	DeviceInfo _cameraInfo;
 };
