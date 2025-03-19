d40e1bfe74398e09cb22f60464f115242272e490
Martin Tuma <martin.tuma@digiteqautomotive.com>
2025-03-19 15:38:39 +0100

    Fixed Qt6 build warnings

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,11 @@ int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
 	qRegisterMetaTypeStreamOperators<StreamInfo>("Stream");
+	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+	QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 #else
 	qRegisterMetaType<StreamInfo>("StreamInfo");
 #endif
-
-	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-	QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 
 	Application app(argc, argv);
 	return app.run();
