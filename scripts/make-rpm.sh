41330447c26a4b397b7f1614383c162588128c8f
Martin Tuma <martin.tuma@digiteqautomotive.com>
2025-04-10 17:35:51 +0200

    Fixed build on latest OpenSUSE Tumbleweed

--- scripts/make-rpm.sh
+++ scripts/make-rpm.sh
@@ -5,6 +5,6 @@
 mkdir -p ~/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 git archive --prefix dqview/ -o dqview.tar.gz HEAD
 mv dqview.tar.gz ~/rpmbuild/SOURCES
-rpmbuild -ba dqview.spec
+QA_RPATHS=0x0001 rpmbuild -ba dqview.spec
 cp ~/rpmbuild/RPMS/`uname -m`/dqview* .
 rm -rf ~/rpmbuild
