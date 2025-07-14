5cf685c3d50f88e4b0adb36b5062ec7009f4f8bf
Martin Tuma <martin.tuma@digiteqautomotive.com>
2025-07-14 18:52:27 +0200

    Added missing audio_filter plugins

--- dqview/scripts/mingw-libs.sh
+++ dqview/scripts/mingw-libs.sh
@@ -5,7 +5,7 @@
 EXE="dqview.exe"
 LIB_DIR="/mingw64/bin"
 VLC_PLUGINS_DIR="/mingw64/lib/vlc/plugins"
-VLC_PLUGINS="access access_output audio_output codec d3d9 d3d11 demux mux packetizer stream_filter stream_out video_filter video_chroma video_output"
+VLC_PLUGINS="access access_output audio_filter audio_output codec d3d9 d3d11 demux mux packetizer stream_filter stream_out video_filter video_chroma video_output"
 QT_PLUGINS_DIR="/mingw64/share/qt5/plugins"
 QT_PLUGINS="platforms/qwindows.dll styles/qwindowsvistastyle.dll"
 OUT_DIR="."
