58826d568957e4066fea6ce8c90b36b7f8cef40b
Martin Tůma <martin.tuma@digiteqautomotive.com>
2025-07-16 13:08:02 +0200

    Remove unused stuff

--- dqview/src/framebuffer.cpp
+++ dqview/src/framebuffer.cpp
@@ -3,11 +3,7 @@
 #include <vlc/vlc.h>
 #include "framebuffer.h"
 
-const REGPINTYPES MediaTypes[1] = {
-	{&MEDIATYPE_Video, &MEDIASUBTYPE_RGB32},
-};
-
-class FrameBufferStream : public CSourceStream, public IAMStreamConfig
+class FrameBufferStream : public CSourceStream
 {
 public:
 	DECLARE_IUNKNOWN;
@@ -15,19 +11,12 @@ public:
 	FrameBufferStream(FrameBuffer::Queue *pQueue, FrameBuffer *pParent,
 	  HRESULT *phr);
 
-	// CSourceStream
 	HRESULT FillBuffer(IMediaSample *pMediaSample);
 	HRESULT DecideBufferSize(IMemAllocator *pIMemAlloc,
 	  ALLOCATOR_PROPERTIES *pProperties);
 	HRESULT SetMediaType(const CMediaType *pMediaType);
 	HRESULT CheckMediaType(const CMediaType *pMediaType);
 	HRESULT GetMediaType(int iPosition, CMediaType *pmt);
-
-	// IAMStreamConfig
-	STDMETHODIMP GetFormat(AM_MEDIA_TYPE **ppmt);
-	STDMETHODIMP SetFormat(AM_MEDIA_TYPE *pmt);
-	STDMETHODIMP GetNumberOfCapabilities(int *piCount, int *piSize);
-	STDMETHODIMP GetStreamCaps(int iIndex, AM_MEDIA_TYPE **ppmt, BYTE *pSCC);
 
 private:
 	FrameBuffer::Queue *m_pQueue;
@@ -172,111 +161,6 @@ HRESULT FrameBufferStream::SetMediaType(const CMediaType *pMediaType)
 
 	return hr;
 }
-
-HRESULT FrameBufferStream::GetFormat(AM_MEDIA_TYPE **ppmt)
-{
-	CheckPointer(ppmt, E_POINTER);
-	const CMediaType *pCurrentMt = &m_mt;
-
-	*ppmt = (AM_MEDIA_TYPE*)CoTaskMemAlloc(sizeof(AM_MEDIA_TYPE));
-	memcpy(*ppmt, pCurrentMt, sizeof(AM_MEDIA_TYPE));
-	if ((*ppmt)->pbFormat != NULL && (*ppmt)->cbFormat > 0) {
-		(*ppmt)->pbFormat = (BYTE*)CoTaskMemAlloc(pCurrentMt->cbFormat);
-		memcpy((*ppmt)->pbFormat, pCurrentMt->pbFormat, pCurrentMt->cbFormat);
-	}
-
-	return S_OK;
-}
-
-HRESULT FrameBufferStream::SetFormat(AM_MEDIA_TYPE *pmt)
-{
-	CheckPointer(pmt, E_POINTER);
-
-	for (size_t i = 0; i < sizeof(MediaTypes) / sizeof(REGPINTYPES); i++) {
-		if (*MediaTypes[i].clsMajorType == pmt->majortype
-		  && *MediaTypes[i].clsMinorType == pmt->subtype) {
-			CMediaType mt;
-			HRESULT hr = GetMediaType(i,&mt);
-
-			if (FAILED(hr))
-				return hr;
-			mt = *pmt;
-			return SetMediaType(&mt);
-		}
-	}
-
-	return E_INVALIDARG;
-}
-
-HRESULT FrameBufferStream::GetNumberOfCapabilities(int *piCount, int *piSize)
-{
-	CheckPointer(piCount, E_POINTER);
-	CheckPointer(piSize, E_POINTER);
-
-	*piCount = sizeof(MediaTypes) / sizeof(REGPINTYPES);
-	*piSize = sizeof(VIDEO_STREAM_CONFIG_CAPS);
-
-	return S_OK;
-}
-
-HRESULT FrameBufferStream::GetStreamCaps(int iIndex, AM_MEDIA_TYPE **ppmt,
-  BYTE *pSCC)
-{
-	CheckPointer(ppmt, E_POINTER);
-
-	CMediaType mt;
-	HRESULT hr = GetMediaType(iIndex, &mt);
-	if (FAILED(hr))
-		return hr;
-
-	*ppmt = (AM_MEDIA_TYPE*)CoTaskMemAlloc(sizeof(AM_MEDIA_TYPE));
-	memcpy(*ppmt, (AM_MEDIA_TYPE*)&mt, sizeof(AM_MEDIA_TYPE));
-
-	if ((*ppmt)->pbFormat!=NULL && (*ppmt)->cbFormat>=sizeof(VIDEOINFOHEADER)) {
-		(*ppmt)->pbFormat = (BYTE*)CoTaskMemAlloc(mt.cbFormat);
-		memcpy((*ppmt)->pbFormat, mt.pbFormat, mt.cbFormat);
-
-		VIDEOINFOHEADER *pvi = (VIDEOINFOHEADER *)(*ppmt)->pbFormat;
-		if (pvi->AvgTimePerFrame==0)
-			pvi->AvgTimePerFrame = 40 * 10000;	// 60Hz in 100ns intervals.
-		if (pvi->dwBitRate==0)
-			pvi->dwBitRate = pvi->bmiHeader.biSizeImage * 8 * 60;
-	}
-
-	if (pSCC) {
-		VIDEO_STREAM_CONFIG_CAPS *pScc = (VIDEO_STREAM_CONFIG_CAPS *)pSCC;
-
-		pScc->guid = mt.formattype;
-		pScc->VideoStandard = 0;
-		pScc->InputSize.cx = m_pQueue->Width();
-		pScc->MaxCroppingSize.cx = m_pQueue->Width();
-		pScc->MinCroppingSize.cx = m_pQueue->Width();
-		pScc->InputSize.cy = m_pQueue->Height();
-		pScc->MaxCroppingSize.cy = m_pQueue->Height();
-		pScc->MinCroppingSize.cy = m_pQueue->Height();
-		pScc->CropGranularityX = 1;
-		pScc->CropGranularityY = 1;
-		pScc->CropAlignX = 1;
-		pScc->CropAlignY = 1;
-		pScc->MaxOutputSize.cx = m_pQueue->Width();
-		pScc->MinOutputSize.cx = m_pQueue->Width();
-		pScc->MaxOutputSize.cy = m_pQueue->Height();
-		pScc->MinOutputSize.cy = m_pQueue->Height();
-		pScc->OutputGranularityX = 1;
-		pScc->OutputGranularityY = 1;
-		pScc->StretchTapsX = 0;
-		pScc->StretchTapsY = 0;
-		pScc->ShrinkTapsX = 0;
-		pScc->ShrinkTapsY = 0;
-		pScc->MaxFrameInterval = 10000000;		// 1Hz
-		pScc->MinFrameInterval = 10000000 / 60;	// 60Hz
-		pScc->MinBitsPerSecond = m_pQueue->Width() * m_pQueue->Height() * 4 * 8;
-		pScc->MaxBitsPerSecond = pScc->MinBitsPerSecond * 60;
-	}
-
-	return S_OK;
-}
-
 
 FrameBuffer::FrameBuffer(int iWidth, int iHeight, int iCapacity, HRESULT *phr)
   : CSource(NAME("Frame Buffer"), NULL, CLSID_FrameBuffer)
